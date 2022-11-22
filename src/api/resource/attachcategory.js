947d7485cca9367e6a5bc4569a97fde80e2cd7da
hzp <110476592@qq.com>
2022-11-22 15:41:20 +0800

    add product comment and resource management

---
+++ src/api/resource/attachcategory.js
@@ -0,0 +1,60 @@
+import request from '@/router/axios';
+
+export const getList = (current, size, params) => {
+  return request({
+    url: '/api/fcb-resource/attach-category/list',
+    method: 'get',
+    params: {
+      ...params,
+      current,
+      size,
+    }
+  })
+}
+
+export const getDetail = (id) => {
+  return request({
+    url: '/api/fcb-resource/attach-category/detail',
+    method: 'get',
+    params: {
+      id
+    }
+  })
+}
+
+export const getTree = (tenantId) => {
+  return request({
+    url: '/api/fcb-resource/attach-category/tree',
+    method: 'get',
+    params: {
+      tenantId,
+    }
+  })
+}
+
+export const remove = (ids) => {
+  return request({
+    url: '/api/fcb-resource/attach-category/remove',
+    method: 'post',
+    params: {
+      ids,
+    }
+  })
+}
+
+export const add = (row) => {
+  return request({
+    url: '/api/fcb-resource/attach-category/submit',
+    method: 'post',
+    data: row
+  })
+}
+
+export const update = (row) => {
+  return request({
+    url: '/api/fcb-resource/attach-category/submit',
+    method: 'post',
+    data: row
+  })
+}
+
