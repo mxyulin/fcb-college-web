05b739d9ea02e820bb445464dfffd886116f07f4
yulin <2247586191@qq.com>
2023-02-03 00:09:57 +0800

    合并冲突

--- vue.config.js
+++ vue.config.js
@@ -28,8 +28,8 @@ module.exports = {
       '/api': {
         //本地服务接口地址
         // target: 'http://192.168.10.210:8090',
-        //target: 'http://127.0.0.1:8090',
-         target: 'http://116.204.112.191:8090',
+        // target: 'http://127.0.0.1:8090',
+        target: 'http://116.204.112.191:8090',
         //远程演示服务地址,可用于直接启动项目
         //target: 'https://fcb.fcbx.vip/api',
         ws: true,
